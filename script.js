6518226d42ad0b6a97ce946575219279f273f12c
Davenof <david.ofili@outlook.com>
2022-12-12 05:29:50 +0000

    Update script.js

--- script.js
+++ script.js
@@ -30,3 +30,21 @@ const updateScore = (winner) => {
 
 // Next round function
 const advanceRound = () => currentRoundNumber++;
+
+//Instructions Accordion
+var acc = document.getElementsByClassName("instructions-btn");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+
+    var panel = this.nextElementSibling;
+    
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
